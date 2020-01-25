feb43f3180860562754603948c4afffc97c742e4
aaa12341 <ahneunseo10@naver.com>
2020-01-26 00:43:06 +0900

    final commit

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+const FooterTag = document.getElementById('foot');
+    FooterTag.innerHTML = ` <footer>
+    <div class="wrap">
+        <p>(주)석세스바로</p>
+        <p>대표: 안경호 | 사업자등록번호: 111-11-111</p>
+        <p>주소: 서울특별시 용산구 한강대로 256, 7층(남영동, 수빌딩)</p>
+        <p>전화: 02-1111-1111 | 팩스: 02-1111-1111</p>
+        <p>Copyright © SuccessBaro ALL RIGHTS RESERVED.</p>
+    </div>
+</footer>
+`
