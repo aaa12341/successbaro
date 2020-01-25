14714a00fb764fa01bee292da72225e130cad28c
aaa12341 <ahneunseo10@naver.com>
2020-01-26 00:32:04 +0900

    commit 1231

--- js/header.js
+++ js/header.js
@@ -9,8 +9,8 @@ const HeaderTag = document.getElementById('head');
                 <li><a href="about.html">About</a></li>
                 <li><a href="incubating1.html">Incubating</a></li>
                 <li><a href="consulting1.html">Consulting</a></li>
-                <li><a href="">HR Consulting</a></li>
-                <li><a href="">Service</a></li>
+                <li><a href="hr.html">HR Consulting</a></li>
+                <li><a href="service1.html">Service</a></li>
                 <li><a href="">Contact</a></li>
             </ul>
         </div>
@@ -29,21 +29,21 @@ const HeaderTag = document.getElementById('head');
             <ul>
                 <li><a href="consulting1.html">정기경영자문</a></li>
                 <li><a href="consulting2.html">경영현황 관리체계</a></li>
-                <li><a href="consulting1.html">BI 기반 경영컨설팅</a></li>
+                <li><a href="consulting3.html">BI 기반 경영컨설팅</a></li>
                 <li><a href="consulting4.html">마케팅 프로세스 구축</a></li>
-                <li><a href="consulting1.html">경영전략 수립</a></li>
+                <li><a href="consulting5.html">경영전략 수립</a></li>
             </ul>
         </div>
         <div class="main_nav hr_nav">
             <ul>
-                <li><a href="consulting1.html">HR 컨설팅</a></li>
+                <li><a href="hr.html">HR 컨설팅</a></li>
             </ul>
         </div>
         <div class="main_nav ser_nav">
             <ul>
-                <li><a href="consulting1.html">ISO 인증 컨설팅</a></li>
-                <li><a href="consulting1.html">감사행정시스템 구축</a></li>
-                <li><a href="consulting1.html">상시감사시스템 구축</a></li>
+                <li><a href="service1.html">ISO 인증 컨설팅</a></li>
+                <li><a href="service1.html">감사행정시스템 구축</a></li>
+                <li><a href="service1.html">상시감사시스템 구축</a></li>
             </ul>
         </div>
     <div>
@@ -69,9 +69,9 @@ $(document).ready(function(){
         $(this).html("<a href='consulting1.html'>Consulting</a>")
     })
     $('.navul li:nth-child(5)').mouseenter(function(){
-        $(this).html("<a href='consulting1.html'>기타 서비스</a>")
+        $(this).html("<a href='service1.html'>기타 서비스</a>")
     })
     $('.navul li:nth-child(5)').mouseleave(function(){
-        $(this).html("<a href='consulting1.html'>Service</a>")
+        $(this).html("<a href='service1.html'>Service</a>")
     })
 })
