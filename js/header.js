ca7d20a3135431c30a8a479a3a893c714a3f552e
aaa12341 <ahneunseo10@naver.com>
2020-01-25 18:36:27 +0900

    commit2

--- js/header.js
+++ js/header.js
@@ -19,7 +19,58 @@ const HeaderTag = document.getElementById('head');
     </div>
 </header>
 <nav>
-    
+    <div class="wrap">
+        <!-- <div class="main_nav about_nav"></div> -->
+        <div class="main_nav incu_nav">
+            <ul>
+                <li><a href="incubating1.html">법인설립 컨설팅</a></li>
+                <li><a href="incubating2.html">사업타당성 분석</a></li>
+                <li><a href="incubating2.html">홈페이지개발</a></li>
+            </ul>
+        </div>
+        <div class="main_nav mento_nav">
+            <ul>
+                <li><a href="mentoring1.html">마케팅 프로세스 구축</a></li>
+                <li><a href="mentoring1.html">사업타당성 분석</a></li>
+                <li><a href="mentoring1.html">홈페이지개발</a></li>
+            </ul>
+        </div>
+        <div class="main_nav mento_nav">
+            <ul>
+                <li><a href="mentoring1.html">월 업무현황 관리체계</a></li>
+                <li><a href="mentoring1.html">경영흐름 분석·예측</a></li>
+                <li><a href="mentoring1.html">매출·손익·자금 분석</a></li>
+                <li><a href="mentoring1.html">재무제표기반 경영분석</a></li>
+                <li><a href="mentoring1.html">마케팅 프로세스 구축</a></li>
+                <li><a href="mentoring1.html">년간 사업계획 수립</a></li>
+                <li><a href="mentoring1.html">경영전략 수립</a></li>
+            </ul>
+        </div>
+        <div class="main_nav mento_nav">
+            <ul>
+                <li><a href="mentoring1.html">채용프로세스 구축</a></li>
+                <li><a href="mentoring1.html">평가체계 구축</a></li>
+                <li><a href="mentoring1.html">임금체계 개선</a></li>
+                <li><a href="mentoring1.html">근로시간제 설계</a></li>
+                <li><a href="mentoring1.html">학습체계 구축</a></li>
+                <li><a href="mentoring1.html">직급체계·승진제도</a></li>
+                <li><a href="mentoring1.html">직원복지제도</a></li>
+                <li><a href="mentoring1.html">퇴직프로그램</a></li>
+            </ul>
+        </div>
+        <div class="main_nav mento_nav">
+            <ul>
+                <li><a href="mentoring1.html">신성장동력 발굴</a></li>
+                <li><a href="mentoring1.html">ERP 구축 컨설팅</a></li>
+            </ul>
+        </div>
+        <div class="main_nav mento_nav">
+            <ul>
+                <li><a href="mentoring1.html">종합진단</a></li>
+                <li><a href="mentoring1.html">맞춤 처방</a></li>
+            </ul>
+        </div>
+    <div>
 </nav>
 `
 $(document).ready(function(){
@@ -30,10 +81,10 @@ $(document).ready(function(){
         $('nav').stop().slideUp()
     })
     $('.navul li:nth-child(1)').mouseenter(function(){
-        $(this).html("<a href='about.html'>어바웃</a>")
+        $(this).html("<a href='about.html'>회사소개</a>")
     })
     $('.navul li:nth-child(1)').mouseleave(function(){
-        $(this).html("<a href='about.html'>about</a>")
+        $(this).html("<a href='about.html'>About</a>")
     })
     $('.navul li:nth-child(2)').mouseenter(function(){
         $(this).html("<a href='incubating1.html'>인큐베이팅</a>")
