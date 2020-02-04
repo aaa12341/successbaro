a3d568986b7b72cce274c5e80ca507cf293485f9
aaa12341 <ahneunseo10@naver.com>
2020-02-04 19:42:12 +0900

    commit

--- js/common.js
+++ js/common.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
     $("#top_btn").html("<div class='fix_btn'>버튼</div>")
+    $(".fix_btn").click(function(){
+        // var introTag = $('.about_section2').offset()
+        $('html,body').animate({
+            scrollTop : 00
+        }, 800)
+    })
+    $("#btn2").click(function(){
+        var introTag = $('.incu_main2').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
+
 })
