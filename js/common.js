9b3cbdf64b91b792c0004b1442c7b582fe1cf1df
aaa12341 <ahneunseo10@naver.com>
2020-02-07 16:12:41 +0900

    commit

--- js/common.js
+++ js/common.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-    $("#top_btn").html("<div class='fix_btn'>버튼</div>")
+    $("#top_btn").html("<div class='fix_btn'>TOP</div>")
     $(".fix_btn").click(function(){
-        // var introTag = $('.about_section2').offset()
         $('html,body').animate({
             scrollTop : 00
         }, 800)
@@ -12,5 +11,58 @@ $(document).ready(function(){
             scrollTop : introTag.top
         }, 800)
     })
-
+    $("#con_btn1").click(function(){
+        var introTag = $('#con_intro1').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
+    $("#con_btn2").click(function(){
+        var introTag = $('#con_intro2').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
+    $("#con_btn3").click(function(){
+        var introTag = $('#con_intro3').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
+    $("#con_btn4").click(function(){
+        var introTag = $('#con_intro4').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
+    $("#con_btn5").click(function(){
+        var introTag = $('#con_intro5').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
+    $("#con_btn6").click(function(){
+        var introTag = $('#con_intro6').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
+    $("#con_btn7").click(function(){
+        var introTag = $('#con_intro7').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
+    $("#ser_btn1").click(function(){
+        var introTag = $('#ser_intro1').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
+    $("#ser_btn2").click(function(){
+        var introTag = $('#ser_intro2').offset()
+        $('html,body').animate({
+            scrollTop : introTag.top
+        }, 800)
+    })
 })
