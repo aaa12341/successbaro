8e287228fa95214364f2b716acc205a13c1a61f2
aaa12341 <ahneunseo10@naver.com>
2020-02-09 15:44:16 +0900

    commit

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,24 @@
-$(window).scroll(function(){
-    var offsets = $(document).scrollTop();
-    var hrManin2 = $('.hr_main2').offset();
-    var hrManin3 = $('.hr_main3').offset();
-    console.log(hrManin2.top)
-    console.log(offsets)
-
-    if(offsets * 3> hrManin2.top){
-        $('#intro_ani div.process').css({
-            "animationName" : "up_fadein"
-        })
-        $('#intro_ani1 div.hr_area').css({
-            "animationName" : "up_fadein"
-        })
-    }
-})
+function hr1(){
+    $(window).scroll(function(){
+        var offsets = $(document).scrollTop();
+        var hrManin2 = $('.hr_main2').offset();
+        if(offsets * 3> hrManin2.top){
+                $('#intro_ani div.process').css({
+                    "animationName" : "up_fadein"
+                })
+                $('#intro_ani1 div.hr_area').css({
+                    "animationName" : "up_fadein"
+                })
+            }
+    })
+}
+function service1(){
+    $(window).scroll(function(){
+        var offsets = $(document).scrollTop();
+        if(offsets > 1200){
+            $('#ser_intro2 div.intro_box').css({
+                "animationName" : "up_fadein"
+            })
+        }
+    })
+}
