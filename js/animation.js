ad229e4322b217b4e8d9a4c17b8e342f29e211bc
aaa12341 <ahneunseo10@naver.com>
2020-02-14 01:38:33 +0900

    commit

--- js/animation.js
+++ js/animation.js
@@ -45,7 +45,7 @@ function hr1(){
     $(window).scroll(function(){
         var offsets = $(document).scrollTop();
         var hrManin2 = $('.hr_main2').offset();
-        if(offsets * 3> hrManin2.top){
+        if(offsets + 800> hrManin2.top){
                 $('#intro_ani div.process').css({
                     "animationName" : "up_fadein"
                 })
