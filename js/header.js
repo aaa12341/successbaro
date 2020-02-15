f8c262e766b4ef2ca748889b8054b7ca956df2f9
aaa12341 <ahneunseo10@naver.com>
2020-02-15 19:52:13 +0900

    commit

--- js/header.js
+++ js/header.js
@@ -7,10 +7,9 @@ const HeaderTag = document.getElementById('head');
             </a>
             <ul class="navul">
                 <li><a href="about.html">About</a></li>
-                <li><a href="incubating1.html">Incubating</a></li>
-                <li><a href="consulting1.html">Consulting</a></li>
-                <li><a href="hr1.html">HR Consulting</a></li>
-                <li><a href="service1.html">Service</a></li>
+                <li><a href="hr.html">경영과 HR</a></li>
+                <li><a href="bdesign.html">Business Design</a></li>
+                <li><a href="levup.html">Level Up.etc</a></li>
                 <li><a href="contact.html">Contact</a></li>
             </ul>
         </div>
@@ -26,33 +25,30 @@ const HeaderTag = document.getElementById('head');
                 <li><a href="about.html?key=about4">오시는 길</a></li>
             </ul>
         </div>
-        <div class="main_nav incu_nav">
-            <ul>
-                <li><a href="incubating1.html">비즈니스 모델 디자인</a></li>
-                <li><a href="incubating1.html?key=incu2">포인트 경영스쿨</a></li>
-                <li><a href="incubating1.html?key=incu3">법인설립 컨설팅</a></li>
-            </ul>
-        </div>
-        <div class="main_nav consul_nav">
-            <ul>
-                <li><a href="consulting1.html">경영 상시자문</a></li>
-                <li><a href="consulting1.html?key=consul2">BI 기반 경영컨설팅</a></li>
-                <li><a href="consulting1.html?key=consul3">상시 경영자문 & <br>경영컨설팅 영역</a></li>
-                <li><a href="consulting1.html?key=consul4">컨설팅 수행 프로세스</a></li>
-                <li><a href="consulting1.html?key=consul5">경영현황 관리체계</a></li>
-                <li><a href="consulting1.html?key=consul6">경영전략 수립</a></li>
-                <li><a href="consulting1.html?key=consul7">마케팅 프로세스 구축</a></li>
-            </ul>
-        </div>
         <div class="main_nav hr_nav">
             <ul>
-                <li><a href="hr.html">HR 컨설팅</a></li>
+                <li><a href="hr.html?key=hr1">경영과 HR</a></li>
+                <li><a href="hr.html?key=hr2">경영 상시자문</a></li>
+                <li><a href="hr.html?key=hr3">경영현황관리체계</a></li>
+                <li><a href="hr.html?key=hr4">HR컨설팅 프로세스</a></li>
+                <li><a href="hr.html?key=hr5">경영컨설팅 프로세스</a></li>
             </ul>
         </div>
-        <div class="main_nav ser_nav">
+        <div class="main_nav bdesign_nav">
             <ul>
-                <li><a href="service1.html">ISO 인증 컨설팅</a></li>
-                <li><a href="service1.html?key=ser2">감사시스템 구축</a></li>
+                <li><a href="bdesign.html?key=bdesign1">비즈니스 모델 디자인</a></li>
+                <li><a href="bdesign.html?key=bdesign2">경영전략 수립</a></li>
+                <li><a href="bdesign.html?key=bdesign3">년간사업계획 수립</a></li>
+                <li><a href="bdesign.html?key=bdesign4">마케팅프로세스</a></li>
+            </ul>
+        </div>
+        <div class="main_nav levup_nav">
+            <ul>
+                <li><a href="levup.html?key=levup1">포인트 경영스쿨</a></li>
+                <li><a href="levup.html?key=levup2">ISO 인증 컨설팅</a></li>
+                <li><a href="levup.html?key=levup3">감사시스템 구축</a></li>
+                <li><a href="levup.html?key=levup4">법인설립 컨설팅</a></li>
+                <li><a href="levup.html?key=levup5">기타 컨설팅</a></li>
             </ul>
         </div>
     <div>
@@ -64,23 +60,5 @@ $(document).ready(function(){
     })
     $('nav, body').mouseleave(function(){
         $('nav').stop().slideUp()
-    })
-    $('.navul li:nth-child(2)').mouseenter(function(){
-        $(this).html("<a href='incubating1.html'>기업 인큐베이팅</a>")
-    })
-    $('.navul li:nth-child(2)').mouseleave(function(){
-        $(this).html("<a href='incubating1.html'>Incubating</a>")
-    })
-    $('.navul li:nth-child(3)').mouseenter(function(){
-        $(this).html("<a href='consulting1.html'>경영 컨설팅</a>")
-    })
-    $('.navul li:nth-child(3)').mouseleave(function(){
-        $(this).html("<a href='consulting1.html'>Consulting</a>")
-    })
-    $('.navul li:nth-child(5)').mouseenter(function(){
-        $(this).html("<a href='service1.html'>기타 서비스</a>")
-    })
-    $('.navul li:nth-child(5)').mouseleave(function(){
-        $(this).html("<a href='service1.html'>Service</a>")
     })
 })
